2129bedf63fda55e2c1d17033753a609ffa4baa4
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-09-11 21:19:31 +0200

    feature(train): register labels datasets + split data (#5)
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * update
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * update
    
    * update
    
    * feature(train-extraction): register extracted dataset
    
    * Update Makefile
    
    * Update Makefile
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(train-extraction): register extracted dataset
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * update
    
    * update
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data
    
    * feature(label_split_data): register labels dataset + split data

--- train/azureml_run_pipeline.py
+++ train/azureml_run_pipeline.py
@@ -1,4 +1,5 @@
 import argparse
+import asyncio
 import json
 import uuid
 
@@ -6,10 +7,11 @@ from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
 
 from azure.ai.ml import MLClient, Input, Output, load_component
 from azure.ai.ml.dsl import pipeline
-from azure.ai.ml.entities import AmlCompute, Environment, BuildContext
+from azure.ai.ml.entities import AmlCompute
 from azure.ai.ml.constants import AssetTypes
 
-from extraction import register_extracted_dataset
+from extraction_dataset import register_extracted_dataset
+from create_labelling_project import DownloadAndCreateLabellingProject, download_and_create_labelling_project
 
 parser = argparse.ArgumentParser("train")
 parser.add_argument("--subscription_id", type=str)
@@ -24,7 +26,6 @@ resource_group_name = args.resource_group_name
 workspace_name = args.workspace_name
 location = args.location
 tags = json.loads(args.tags)
-
 
 try:
     credential = DefaultAzureCredential()
@@ -50,18 +51,26 @@ cluster_basic = AmlCompute(
     name=cluster_name,
     type="amlcompute",
     size="Standard_D4s_v3",
-    location=location, #az account list-locations -o table
+    location=location,  # az account list-locations -o table
     min_instances=0,
     max_instances=1,
     idle_time_before_scale_down=60,
 )
 ml_client.begin_create_or_update(cluster_basic).result()
 
+
 @pipeline(default_compute=cluster_name)
-def azureml_pipeline(pdfs_input_data: Input(type=AssetTypes.URI_FOLDER)):
+def azureml_pipeline(pdfs_input_data: Input(type=AssetTypes.URI_FOLDER),
+                     labels_input_data: Input(type=AssetTypes.URI_FOLDER), ):
     extraction_step = load_component(source="extraction/command.yaml")
     extraction = extraction_step(
         pdfs_input=pdfs_input_data
+    )
+    label_split_data_step = load_component(source="label_split_data/command.yaml")
+    label_split_data = label_split_data_step(
+        labels_input=labels_input_data,
+        pdfs_input=pdfs_input_data,
+        images_input=extraction.outputs.images_output,
     )
 
     output_step = load_component(source="output/command.yaml")
@@ -78,13 +87,25 @@ def azureml_pipeline(pdfs_input_data: Input(type=AssetTypes.URI_FOLDER)):
 pipeline_job = azureml_pipeline(
     pdfs_input_data=Input(
         path="azureml:cats_dogs_others:1", type=AssetTypes.URI_FOLDER
-    )
+    ),
+    labels_input_data=Input(
+        path="azureml:cats_dogs_others_labels:1", type=AssetTypes.URI_FOLDER
+    ),
 )
 
 azure_blob = "azureml://datastores/workspaceblobstore/paths/"
 experiment_id = str(uuid.uuid4())
 custom_output_path = (
-    azure_blob + "extraction/cats-dogs-others/" + experiment_id + "/"
+        azure_blob + "extraction/cats-dogs-others/" + experiment_id + "/"
+)
+pipeline_job.outputs.output = Output(
+    type=AssetTypes.URI_FOLDER, mode="rw_mount", path=custom_output_path
+)
+
+azure_blob = "azureml://datastores/workspaceblobstore/paths/"
+experiment_id = str(uuid.uuid4())
+custom_output_path = (
+        azure_blob + "extraction/cats-dogs-others/" + experiment_id + "/"
 )
 pipeline_job.outputs.output = Output(
     type=AssetTypes.URI_FOLDER, mode="rw_mount", path=custom_output_path
@@ -92,11 +113,30 @@ pipeline_job.outputs.output = Output(
 
 pipeline_job = ml_client.jobs.create_or_update(
     pipeline_job, experiment_name="cats_dos_others_pipeline",
-    tags=tags
+    tags={**tags, experiment_id: experiment_id}
 )
 
 ml_client.jobs.stream(pipeline_job.name)
 
-register_extracted_dataset(
-    ml_client, custom_output_path, tags
+registered_dataset = register_extracted_dataset(
+    ml_client,
+    custom_output_path,
+    {**tags, experiment_id: experiment_id},
 )
+
+if registered_dataset is not None:
+    create_project = DownloadAndCreateLabellingProject(
+        subscription_id=subscription_id,
+        resource_group_name=resource_group_name,
+        workspace_name=workspace_name,
+    )
+
+    async def execute_async():
+        await download_and_create_labelling_project(
+            registered_dataset.dataset_version,
+            registered_dataset.dataset_name,
+            create_project,
+        )
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(execute_async())
--- train/extraction_dataset.py
+++ train/extraction_dataset.py
@@ -1,38 +1,47 @@
+import asyncio
+from dataclasses import dataclass
 from pathlib import Path
 import azure.ai.ml._artifacts._artifact_utilities as artifact_utils
 from azure.ai.ml.constants import AssetTypes
 from azure.ai.ml.entities import Data
 
 
+@dataclass
+class RegisterExtractedDataset:
+    dataset_version: str
+    dataset_name: str
+
+
 def register_extracted_dataset(ml_client,
                                custom_output_path: str,
-                               tags: dict):
-    BASE_PATH = Path(__file__).resolve().parent
+                               tags: dict) -> RegisterExtractedDataset | None:
+    base_path = Path(__file__).resolve().parent
+
     artifact_utils.download_artifact_from_aml_uri(
         uri=custom_output_path + "extraction_hash",
-        destination=str(BASE_PATH),
+        destination=str(base_path),
         datastore_operation=ml_client.datastores,
     )
 
-    # lire le fichier hash.txt qui est dans BASE_PATH
-    with open(str(BASE_PATH / "hash.txt"), "r") as file:
+    # lire le fichier hash.txt qui est dans base_path
+    with open(str(base_path / "hash.txt"), "r") as file:
         computed_hash = file.read()
     print(f"computed_hash: {computed_hash}")
 
     extracted_images_dataset_name = "cats-dogs-others-extracted"
     try:
         list_datasets = ml_client.data.list(extracted_images_dataset_name)
-        list_list_datset = list(list_datasets)
-        version_dataset_extraction = len(list_list_datset) + 1
+        list_list_dataset = list(list_datasets)
+        version_dataset_extraction = len(list_list_dataset) + 1
     except:
-        list_list_datset = []
+        list_list_dataset = []
         version_dataset_extraction = 1
         print("No dataset with name cats-dogs-others-extracted")
 
     hash_tag_already_exists = False
-    len_dataset = len(list_list_datset)
+    len_dataset = len(list_list_dataset)
     if len_dataset > 0:
-        dataset = list_list_datset[len_dataset - 1]
+        dataset = list_list_dataset[len_dataset - 1]
         print(f"dataset.tags: {str(dataset.version)}")
         print(dataset.tags)
         if "hash" in dataset.tags:
@@ -42,16 +51,20 @@ def register_extracted_dataset(ml_client,
             if extracted_images_dataset_version == computed_hash:
                 hash_tag_already_exists = True
 
-    if not hash_tag_already_exists:
+    if not hash_tag_already_exists and "git_head_ref" in tags:  # and tags["git_head_ref"] == "main":
         extracted_images_dataset = Data(
             name=extracted_images_dataset_name,
             path=custom_output_path + "extraction_images",
-            type=AssetTypes.CUSTOM_MODEL,
+            type=AssetTypes.URI_FOLDER,
             description="Extracted images for cats and dogs and others",
             version=str(version_dataset_extraction),
             tags={"hash": computed_hash, **tags},
         )
         extracted_images_dataset = ml_client.data.create_or_update(extracted_images_dataset)
         print(
-            f"Dataset with name {extracted_images_dataset.name} was registered to workspace, the dataset version is {extracted_images_dataset.version}"
+            f"Dataset with name {extracted_images_dataset_name} was registered to workspace, the dataset version is {extracted_images_dataset.version}"
         )
+        return RegisterExtractedDataset(dataset_version=extracted_images_dataset.version,
+                                        dataset_name=extracted_images_dataset_name)
+
+    return None
